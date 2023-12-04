2a1629559912fdc403ce47ed0920e84a67a172e4
meokisama <hoangdinhsang123@gmail.com>
2023-12-04 23:35:06 +0700

    homepage for books choosing

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("9784798633336").addEventListener("click", function () {
+    window.open("/content/9784798633336/index.html", '_blank');
+});
+
+
+document.getElementById("9784798633350").addEventListener("click", function () {
+    window.open("/content/9784798633350/index.html", '_blank');
+});
+
+
+document.getElementById("9784046821096").addEventListener("click", function () {
+    window.open("/content/9784046821096/index.html", '_blank');
+});
