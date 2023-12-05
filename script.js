941c627757000ed2338321ed3ee62869a5d41f96
meokisama <hoangdinhsang123@gmail.com>
2023-12-05 16:04:12 +0700

    fixed relative urls

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 document.getElementById("9784798633336").addEventListener("click", function () {
-    window.open("content/9784798633336/index.html", '_blank');
+    window.open("content/9784798633336/", '_blank');
 });
 
 
 document.getElementById("9784798633350").addEventListener("click", function () {
-    window.open("content/9784798633350/index.html", '_blank');
+    window.open("content/9784798633350/", '_blank');
 });
 
 
 document.getElementById("9784046821096").addEventListener("click", function () {
-    window.open("content/9784046821096/index.html", '_blank');
+    window.open("content/9784046821096/", '_blank');
 });
 
 
 document.getElementById("9784046826398").addEventListener("click", function () {
-    window.open("content/9784046826398/index.html", '_blank');
+    window.open("content/9784046826398/", '_blank');
 });
 
 
 document.getElementById("9784041140628").addEventListener("click", function () {
-    window.open("content/9784041140628/index.html", '_blank');
+    window.open("content/9784041140628/", '_blank');
 });
 
 
 document.getElementById("9784049150148").addEventListener("click", function () {
-    window.open("content/9784049150148/index.html", '_blank');
+    window.open("content/9784049150148/", '_blank');
 });
 
 
 document.getElementById("9784046827746").addEventListener("click", function () {
-    window.open("content/9784046827746/index.html", '_blank');
+    window.open("content/9784046827746/", '_blank');
 });
 
 
 document.getElementById("9784867164518").addEventListener("click", function () {
-    window.open("content/9784867164518/index.html", '_blank');
+    window.open("content/9784867164518/", '_blank');
 });
